3dcd40cca18aa030fba36b191528bf7f0a8d31e7
daffalde <daffalderayhan@gmail.com>
2026-06-25 20:07:53 +0700

    setup project

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./dashboard.module.css";
+
+export default function Dashboard() {
+  return (
+    <>
+      <div className={styles.container_dashboard}>
+        <h1>dashboard</h1>
+      </div>
+    </>
+  );
+}
