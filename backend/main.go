f4d76d4ccba02865e2883e9d52afb81afcf878a6
daffalde <daffalderayhan@gmail.com>
2026-08-24 14:55:58 +0700

    feat:congfiguration docker for postgre and golang

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"backend/routes"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	godotenv.Load()
 	fmt.Println("Server running....")
+	r := routes.IntegratedRoute()
+	r.Run(os.Getenv("PORT"))
 }
