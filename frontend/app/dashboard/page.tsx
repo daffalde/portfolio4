58963e39ac1c868d9b1fb60380f9e770a435838c
daffalde <daffalderayhan@gmail.com>
2026-08-25 10:23:14 +0700

    feat:adding dashboard home page and navigantion bar

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,7 +1,23 @@
+import DbNavbar from "@/components/DbNavbar";
+import "../../styles/dashboard/dashboardGlobal.css";
+import styles from "../../styles/dashboard/dbHome.module.css";
+
 export default function DashboardHome() {
   return (
     <>
-      <h1>Dashboard</h1>
+      <div className="db-body">
+        <div className="db-page">
+          <DbNavbar />
+          <div className="db-content">
+            <div className="db-c-header">
+              <h2>Dashboard</h2>
+            </div>
+            <div className={styles.homeContent}>
+              <p className="p-second">No data available yet.</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
---
+++ frontend/components/DbNavbar.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import styles from "./dbNavbar.module.css";
+import Link from "next/link";
+
+export default function DbNavbar() {
+  const getPath = usePathname();
+  const navbarData = [
+    {
+      name: "/dashboard",
+      image: "home.png",
+      link: "#",
+    },
+    {
+      name: "/dashboard/project",
+      image: "project.png",
+      link: "#",
+    },
+    {
+      name: "/dashboard/skill",
+      image: "skill.png",
+      link: "#",
+    },
+    {
+      name: "/dashboard/message",
+      image: "message.png",
+      link: "#",
+    },
+    {
+      name: "/dashboard/project?add=true",
+      image: "add.png",
+      link: "#",
+    },
+    {
+      name: "link",
+      image: "link.png",
+      link: "#",
+    },
+  ];
+  return (
+    <>
+      <div className={styles.body}>
+        <img src="/short-logo.png" alt="website logo" />
+        <div className={`${styles.main}`}>
+          <div className={styles.bg}>
+            {navbarData.slice(0, 4).map((e, i) => (
+              <Link key={i} href={e.name}>
+                <button
+                  className={`${styles.button} ${getPath == e.name ? styles.buttonSelected : ""}`}
+                >
+                  <img src={`/${e.image}`} alt="navbar icon" />
+                </button>
+              </Link>
+            ))}
+          </div>
+          <div className={styles.bg}>
+            <button className={`${styles.button} `}>
+              <img src={"/add.png"} alt="add icon" />
+            </button>
+            <button
+              onClick={() => window.open("https://www.google.com/")}
+              className={`${styles.button} `}
+            >
+              <img src={"/link.png"} alt="link icon" />
+            </button>
+          </div>
+        </div>
+        <div className={styles.bg}>
+          <button className={styles.button}>
+            <img src="/logout.png" alt="logout icon" />
+          </button>
+          <button
+            className={`${styles.button} ${styles.profil}`}
+            style={{
+              backgroundImage: 'url("/db-bg.jpg")',
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          ></button>
+        </div>
+      </div>
+    </>
+  );
+}
