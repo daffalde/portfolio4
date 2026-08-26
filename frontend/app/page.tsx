768d2c5363c8ba4ec01ece10a3f90efb7e1bc810
daffalde <daffalderayhan@gmail.com>
2026-08-26 20:38:25 +0700

    feat:adding vercel analytics

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function Home() {
   return (
     <>
       <div className="homepage">
+        <Analytics />
         <img width={"100px"} src="/cone.png" alt="working progress logo" />
         <h4>Work in Progress </h4>
         <p>We are still developing this site, so expect ongoing updates.</p>
